d53769b6373813f465a498ac95051daeb911fcb2
syz <syz@163.com>
2020-07-15 09:31:22 +0800

    del--

--- NLP-文本分类/code/text_classifier_tf/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .textcnn import TextCnnModel
-from .bilstm import BiLstmModel
-from .bilstmatten import BiLstmAttenModel
-from .rcnn import RcnnModel
-from .transformer import TransformerModel
-from .han import HANModel
-
-
-__all__ = ["TextCnnModel", "BiLstmModel", "BiLstmAttenModel", "RcnnModel", "TransformerModel", "HANModel"]
